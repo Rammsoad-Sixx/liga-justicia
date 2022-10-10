2c4a7a496f6e1539ac65c9350191e2a3426bbcf1
RafaelWize <rafael.ruiz@wizeline.com>
2022-10-10 15:27:12 -0700

    artifactory curl uncommented

--- .circleci/publish_package.sh
+++ .circleci/publish_package.sh
@@ -5,10 +5,10 @@ set -ex
 HOST="https://rafapaz.jfrog.io/artifactory/composer-php-local"
 # Determine the version from the tag; remove the leading v "v3.3.0" -> "3.3.0"
 LATEST_TAG=$(git describe --tags --abbrev=0)
-VERSION=2.0.0
+VERSION=3.0.0
 
 FILE_NAME="rafapaz-test-$VERSION.zip"
 git archive -o "$FILE_NAME" HEAD
 
 # curl al rest api
-# curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/circleci/welkio-error/$FILE_NAME" -T "$FILE_NAME"
+ curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/circleci/welkio-error/$FILE_NAME" -T "$FILE_NAME"
