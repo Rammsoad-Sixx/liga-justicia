3210a65941fd5297801fdaac2854c2fe7b5cc90d
RafaelWize <rafael.ruiz@wizeline.com>
2022-10-11 13:56:19 -0700

    obtener automaticamente el numero de versión en github

--- .circleci/publish_package.sh
+++ .circleci/publish_package.sh
@@ -4,8 +4,8 @@ set -ex
 # HOST="https://wework.jfrog.io/artifactory/api/composer/php-local"
 HOST="https://rafapaz.jfrog.io/artifactory/composer-php-local"
 # Determine the version from the tag; remove the leading v "v3.3.0" -> "3.3.0"
-LATEST_TAG=$(git describe --tags --abbrev=0)
-VERSION=8.2.0
+VERSION=$(git describe --tags --abbrev=0)
+
 
 FILE_NAME="rafapaz-test-$VERSION.zip"
 git archive -o "$FILE_NAME" HEAD
